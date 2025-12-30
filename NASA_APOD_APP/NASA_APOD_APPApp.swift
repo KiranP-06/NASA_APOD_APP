e0a9744b302903a0a8eed27dedd64c0d6666a858
KiranP-06 <kiranpriyadarshini06@gmail.com>
2025-12-30 10:30:18 +0530

    Initial Commit

---
+++ NASA_APOD_APP/NASA_APOD_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  NASA_APOD_APPApp.swift
+//  NASA_APOD_APP
+//
+//  Created by admin34 on 30/12/25.
+//
+
+import SwiftUI
+
+@main
+struct NASA_APOD_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
