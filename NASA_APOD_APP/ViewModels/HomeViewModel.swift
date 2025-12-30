cdfad82a657aac931b37f202703457f7cc55d435
KiranP-06 <kiranpriyadarshini06@gmail.com>
2025-12-30 10:41:58 +0530

    HomeViewModel.swift
    This connects your UI to the data and handles the state.

---
+++ NASA_APOD_APP/ViewModels/HomeViewModel.swift
@@ -0,0 +1,46 @@
+//
+//  HomeViewModel.swift
+//  NASA_APOD_APP
+//
+//  Created by admin34 on 30/12/25.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+@MainActor
+class HomeViewModel: ObservableObject {
+    @Published var apodItem: APODItem?
+    @Published var isLoading: Bool = false
+    @Published var errorMessage: String?
+    @Published var selectedDate: Date = Date()
+    
+    private let service = APIService()
+    
+    // Constraints as per requirements
+    let minDate = Calendar.current.date(from: DateComponents(year: 1995, month: 6, day: 16))!
+    let maxDate = Date()
+    
+    func loadAPOD() async {
+        isLoading = true
+        errorMessage = nil
+        
+        do {
+            let item = try await service.fetchAPOD(date: selectedDate)
+            self.apodItem = item
+        } catch {
+            self.errorMessage = "Failed to load image. Please check your connection."
+            
+        }
+        
+        isLoading = false
+    }
+    
+    
+    func onChangeDate() {
+        Task {
+            await loadAPOD()
+        }
+    }
+}
