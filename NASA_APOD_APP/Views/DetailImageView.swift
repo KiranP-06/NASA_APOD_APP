5407504bdeec668c49ea6a1445bd1373afbbfde8
KiranP-06 <kiranpriyadarshini06@gmail.com>
2025-12-30 10:45:06 +0530

    Create DetailImageView.swift
    Image Detail View with Pinch to Zoom

---
+++ NASA_APOD_APP/Views/DetailImageView.swift
@@ -0,0 +1,40 @@
+//
+//  DetailImageView.swift
+//  NASA_APOD_APP
+//
+//  Created by admin34 on 30/12/25.
+//
+
+import SwiftUI
+
+struct DetailImageView: View {
+    let imageURL: URL
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        ZStack(alignment: .topTrailing) {
+            // Basic Zoomable ScrollView
+            GeometryReader { proxy in
+                ScrollView([.horizontal, .vertical]) {
+                    AsyncImage(url: imageURL) { image in
+                        image
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: proxy.size.width, height: proxy.size.height)
+                    } placeholder: {
+                        ProgressView()
+                    }
+                }
+            }
+            
+            // Close Button
+            Button(action: { dismiss() }) {
+                Image(systemName: "xmark.circle.fill")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                    .padding()
+            }
+        }
+        .background(Color.black)
+    }
+}
